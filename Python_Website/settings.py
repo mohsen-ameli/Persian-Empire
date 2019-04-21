7664f15e7adc858e4f042ae01107b15c83b5cebc
Mohsen Ameli <mohsen.ameli83@gmail.com>
2019-04-21 16:48:00 -0400

    change 9

--- Python_Website/settings.py
+++ Python_Website/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = False
+# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['persiaempire.herokuapp.com']
 
