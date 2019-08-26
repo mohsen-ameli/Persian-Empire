411a76e2dda2e6732e793540be372d6ca0cc7278
Mohsen Ameli <mohsen.ameli83@gmail.com>
2019-08-26 14:57:45 -0400

    Initilize

---
+++ blog/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView,
+)
+
+urlpatterns = [
+    path('', PostListView.as_view (), name='home-blog'),
+    path('user/<str:username>', UserPostListView.as_view (), name='user-post'),
+    path('post/<int:pk>', PostDetailView.as_view (), name='post-detail'),
+    path('post/new/', PostCreateView.as_view (), name='new-post'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view (), name='update-post'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view (), name='delete-post'),
+    path ('about/', views.About, name='blog-about')
+]
